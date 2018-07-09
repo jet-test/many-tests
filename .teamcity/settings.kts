48c23a74937848b76b384817cb271d2f1ce81206
aleksey.dobrynin <aleksey.dobrynin@jetbrains.com>
2018-07-09 22:14:49 +0700

    add build configuration

---
+++ .teamcity/settings.kts
@@ -0,0 +1,45 @@
+import jetbrains.buildServer.configs.kotlin.v2018_1.*
+import jetbrains.buildServer.configs.kotlin.v2018_1.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.v2018_1.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2018_1.vcs.GitVcsRoot
+
+version = "2018.1"
+
+project {
+
+    val vcs = GitVcsRoot{
+        name = "https://github.com/jet-test/many-tests.git#refs/heads/master"
+        url = "https://github.com/jet-test/many-tests.git"
+    }
+
+    vcsRoot(vcs)
+
+    buildType {
+        name = "Build"
+
+        vcs {
+            root(vcs)
+        }
+
+        steps {
+            maven {
+                goals = "clean install exec:java"
+                mavenVersion = custom {
+                    path = "/usr/local/Cellar/maven/3.5.4/libexec"
+                }
+            }
+            maven {
+                goals = "clean test"
+                pomLocation = "mytest/pom.xml"
+                mavenVersion = custom {
+                    path = "/usr/local/Cellar/maven/3.5.4/libexec"
+                }
+            }
+        }
+
+        triggers {
+            vcs {
+            }
+        }
+    }
+}
